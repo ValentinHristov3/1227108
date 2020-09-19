c0dc57c1fc27c3db53b1ddee74f3ee10bb52777b
ValentinHristov3 <valentin.hristov33@gmail.com>
2020-09-19 09:45:00 +0300

    updating progress

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php
 echo "Hello";
+$username ="ValentinHristov3";
+echo $username;
+echo 'My name is $username <br>';
+$age = 22;
+echo $age;
+$istrue = true;
+echo $istrue;
+$usernameArray= ["Alex" ,"Blake"];
+echo $usernameArray[0];
+$usernameAsocArray = ["username"=>"Valentin" , "age"=>22];
+echo $usernameAsocArray["username"];
+echo $usernameAsocArray["age"];
+for($i=0;$i<2;$i++)
+{
+    echo $usernameArray[$i];
+}
+foreach($usernameAsocArray as $key=>$item)
+{
+    echo $key." ".$item."<br>";
+}
