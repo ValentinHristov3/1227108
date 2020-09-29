65d9993f78e22eb67f5ed5392be9d43f892d1cfb
mixaelaavramova <mixaela.avramova@gmail.com>
2020-09-29 08:56:15 +0300

    if/else functions

--- index.php
+++ index.php
@@ -1,22 +1,74 @@
 <?php
-echo "Hello";
-$username ="ValentinHristov3";
-echo $username;
-echo 'My name is $username <br>';
-$age = 22;
-echo $age;
-$istrue = true;
-echo $istrue;
-$usernameArray= ["Alex" ,"Blake"];
-echo $usernameArray[0];
-$usernameAsocArray = ["username"=>"Valentin" , "age"=>22];
-echo $usernameAsocArray["username"];
-echo $usernameAsocArray["age"];
-for($i=0;$i<2;$i++)
-{
-    echo $usernameArray[$i];
-}
-foreach($usernameAsocArray as $key=>$item)
-{
-    echo $key." ".$item."<br>";
-}
+// upr1
+// echo "Hello";
+// $username ="ValentinHristov3";
+// echo $username;
+// echo 'My name is $username <br>';
+// $age = 22;
+// echo $age;
+// $istrue = true;
+// echo $istrue;
+// $usernameArray= ["Alex" ,"Blake"];
+// echo $usernameArray[0];
+// $usernameAsocArray = ["username"=>"Valentin" , "age"=>22];
+// echo $usernameAsocArray["username"];
+// echo $usernameAsocArray["age"];
+// for($i=0;$i<2;$i++)
+// {
+//     echo $usernameArray[$i];
+// }
+// foreach($usernameAsocArray as $key=>$item)
+// {
+//     echo $key." ".$item."<br>";
+// }
+
+
+// upr2
+
+// $username = "smth";
+// if($username == "smth")
+// {
+//     echo "Something";
+// }
+
+// if(2<1)
+// {
+//     echo "Inside if";
+// }
+// else if (2>1)
+// {
+//     echo "Inside else if";
+// }
+// else {
+//     echo "something";
+// }
+
+// $username = "Mitko";
+// switch($username)
+// {
+//     case $username == "Ivan":
+//     echo "he is Ivan";
+// break;
+//     case $username == "Mitko":
+//     echo "he is Mitko";
+// break;
+// default;
+// echo "i don't know the name";
+// break;
+// }
+
+// function printName($username){
+//     echo $username;
+// }
+// printName("Olivia");
+
+// function IsThisOlivia($username)
+// {
+//     if($username == "Olivia"){
+//         echo "This is Olivia";
+//     } else {
+//         echo "This is not Olivia";
+//     }
+// }
+// IsThisOlivia("Ivan");
+
